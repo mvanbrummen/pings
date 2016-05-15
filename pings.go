e2a723d2c90d01b6be1d289a22ef71cc4e474f45
Michael Van Brummen <michaelvanbrummen@gmail.com>
2016-05-15 20:03:15 +1000

    Setup server/static html dir

--- pings.go
+++ pings.go
@@ -2,8 +2,50 @@ package main
 
 import (
     "fmt"
+    "log"
+    "net/http"
+    //"html"
+
+    "github.com/gorilla/mux"
 )
 
-func main() {
-    fmt.Print("Hello, world!\n")
+func StorePing(w http.ResponseWriter, r *http.Request) {
+
 }
+
+func RetrievePing(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, r.URL.Path)
+    vars := mux.Vars(r)
+    id := vars["deviceId"]
+    fmt.Fprintf(w, "\ndeviceId: %v", id)
+    from := vars["from"]
+    fmt.Fprintf(w, "\nfrom: %v", from)
+    to := vars["to"]
+    fmt.Fprintf(w, "\nto: %v", to)
+}
+
+func RetrieveDevices(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Get all devices")
+}
+
+func DeleteAllPings(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Deleted all pings")
+}
+
+func main() {
+    // initialise router
+    router := mux.NewRouter().StrictSlash(true)
+   
+    // serve up static html
+    router.Handle("/", http.FileServer(http.Dir("./static")))
+
+    // routes
+    router.HandleFunc("/test", StorePing)
+    router.HandleFunc("/{deviceId}/{from}/{to}", RetrievePing)    
+    router.HandleFunc("/devices", RetrieveDevices)
+    router.HandleFunc("/clear_data", DeleteAllPings)
+
+    // start server
+    log.Fatal(http.ListenAndServe(":3000", router))
+}
+
