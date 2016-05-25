c731a8b2344ff50a5c58ccd07b3176403b355920
Michael Van Brummen <michaelvanbrummen@gmail.com>
2016-05-25 15:44:52 +1000

    Adding date format check

--- handlers.go
+++ handlers.go
@@ -8,6 +8,7 @@ import (
     "strconv"
     "os"
     "sort"
+    "regexp"
 
     "github.com/gorilla/mux"
 )
@@ -15,34 +16,48 @@ import (
 const (
     DataFile string = "ping.data"
     AllDevices string = "all"
+    fmtISO = "^\\d{4}-\\d{2}-\\d{2}$"
+    fmtUnix = "^[0-9]\\d*$"
 )
+
+func isDateFormat(format, date string) bool {
+    b, err := regexp.MatchString(format, date)
+    if err != nil {
+        return false
+    }
+    return b
+}
 
 func StorePing(w http.ResponseWriter, r *http.Request) {
     // get path params
     vars := mux.Vars(r)
     id := vars["deviceId"]
     time := vars["epochTime"]
-    t, _ := strconv.ParseInt(time, 0, 64)
+    if isDateFormat(fmtUnix, time) {
+        t, _ := strconv.ParseInt(time, 0, 64)
 
-    // get data from file otherwise instantiate
-    var m map[string]int64arr;
-    if PingMapCreated() {
-   	m, _ = GetPingMap()
+        // get data from file otherwise instantiate
+        var m map[string]int64arr;
+        if PingMapCreated() {
+   	    m, _ = GetPingMap()
+        } else {
+            m = make(map[string]int64arr)
+        }
+
+        // append time to slice
+        m[id] = append(m[id], int64(t))
+
+        // sort slice
+        sort.Sort(m[id])
+
+        // write data to file
+        b, _ := GetBytes(m)
+        err := ioutil.WriteFile(DataFile, b, 0600)  
+        if err != nil {
+            fmt.Printf("Failed to write data.")
+        }
     } else {
-        m = make(map[string]int64arr)
-    }
-
-    // append time to slice
-    m[id] = append(m[id], int64(t))
-
-    // sort slice
-    sort.Sort(m[id])
-
-    // write data to file
-    b, _ := GetBytes(m)
-    err := ioutil.WriteFile(DataFile, b, 0600)  
-    if err != nil {
-        fmt.Printf("Failed to write data.")
+	fmt.Printf("Epoch time was not a unix timestamp.\n")
     }
 }
 
@@ -51,34 +66,37 @@ func RetrievePing(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
     id := vars["deviceId"]
     from := vars["from"]
-
-    m, err := GetPingMap()
-    if err != nil {
-	fmt.Printf("Could not get ping map")
-    }
-
-    if id == AllDevices {
-	// filter on every slice
-        devices, err := GetPingMapKeys()
+    if isDateFormat(fmtISO, from) {
+        m, err := GetPingMap()
         if err != nil {
-	    fmt.Printf("Failed to get ping map keys.")
+	    fmt.Printf("Could not get ping map")
         }
-   	for _, k := range devices {
- 	    m[k] = m[k].Pings(from)
-  	}	
-        pingResponse, err := json.Marshal(m)
-        if err != nil {
-            fmt.Printf("Failed to marshal to json")
-        }
-        fmt.Fprintf(w, "%s", pingResponse)
+
+        if id == AllDevices {
+	    // filter on every slice
+            devices, err := GetPingMapKeys()
+            if err != nil {
+	        fmt.Printf("Failed to get ping map keys.")
+            }
+   	    for _, k := range devices {
+ 	        m[k] = m[k].Pings(from)
+  	    }	
+            pingResponse, err := json.Marshal(m)
+            if err != nil {
+                fmt.Printf("Failed to marshal to json")
+            }
+            fmt.Fprintf(w, "%s", pingResponse)
+        } else {
+ 	    // filter on the specified slice
+            m[id] = m[id].Pings(from)
+            pingResponse, err := json.Marshal(m[id])
+            if err != nil {
+                fmt.Printf("Failed to marshal to json")
+            }
+            fmt.Fprintf(w, "%s", pingResponse)
+        } 
     } else {
- 	// filter on the specified slice
-        m[id] = m[id].Pings(from)
-        pingResponse, err := json.Marshal(m[id])
-        if err != nil {
-            fmt.Printf("Failed to marshal to json")
-        }
-        fmt.Fprintf(w, "%s", pingResponse)
+	fmt.Printf("From was not an ISO date\n")
     }
 }
 
